6d500ab35eb5819d55213468880e9a84cd850c84
Miles Wells <k1o0@3tk.co>
2021-08-16 19:11:28 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='ONE-api',
-    version='0.5.2',
+    version='0.5.3',
     python_requires='>={}.{}'.format(*REQUIRED_PYTHON),
     description='Open Neurophysiology Environment',
     license="MIT",
