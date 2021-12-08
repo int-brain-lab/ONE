1daa1430d55eb9ec0c13d5c5a2cb300ede644d47
Miles Wells <k1o0@3tk.co>
2021-12-08 12:20:28 +0200

    Update version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='ONE-api',
-    version='1.6.2',
+    version='1.6.3',
     python_requires='>={}.{}'.format(*REQUIRED_PYTHON),
     description='Open Neurophysiology Environment',
     license="MIT",
