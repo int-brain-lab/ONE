48396f68fe55aabf46b894817bb912344b0153ab
olivier <olivier.winter@hotmail.fr>
2022-01-24 09:41:01 +0000

    bump version number

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='ONE-api',
-    version='1.7.1',
+    version='1.7.2',
     python_requires='>={}.{}'.format(*REQUIRED_PYTHON),
     description='Open Neurophysiology Environment',
     license="MIT",
